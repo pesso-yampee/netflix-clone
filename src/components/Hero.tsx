fd1ceeb1e10ef154f4ff3ad88111ec995c09f950
yamasaki junpei <opss0622@gmail.com>
2023-03-25 10:08:54 +0900

    ✨ ヒーロー部分にグラデーションを追加

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -33,15 +33,15 @@ export const Hero = (): JSX.Element => {
   }, []);
   return (
     <div
-      style={{ backgroundImage: `url(${imgSrc})` }}
-      className=" relative block aspect-video w-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${imgSrc})`, height: "50vh" }}
+      className=" relative flex w-screen items-center bg-cover bg-center bg-no-repeat before:absolute before:bottom-0 before:block before:h-1/4 before:w-full before:bg-gradient-to-b before:from-transparent before:to-black"
     >
       <div className="max-w-sm">
         <h1 className="text-2xl text-white">
           {movie.name ? movie.name : movie.title}
         </h1>
         <div className="mt-1 flex gap-4">
-          <Button text="play" />
+          <Button text="Play" />
           <Button text="My List" />
         </div>
         <p className="mt-4 text-sm text-white">{movie.overview}</p>
