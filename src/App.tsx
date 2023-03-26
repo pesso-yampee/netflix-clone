b9a4d0edaa853bff0da4a33f62815233eb912823
yamasaki junpei <opss0622@gmail.com>
2023-03-26 09:31:21 +0900

    💄 高さは100vhを保持するスタイルを追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,19 @@ import { Row } from "components/Row";
 import { Hero } from "components/Hero";
 export default function App(): JSX.Element {
   return (
-    <>
+    <div className="bg-black min-h-screen">
       <Hero />
-      <div className="bg-black">
-        <Row
-          title="NETFLIX ORIGINALS"
-          fetchUrl={requests.categoryNetflixOriginals}
-          isLargeRow={true}
-        />
-        <Row title="Top Movies" fetchUrl={requests.categoryTopRated} />
-        <Row title="Action Movies" fetchUrl={requests.categoryActionMovies} />
-        <Row title="Comedy Movies" fetchUrl={requests.categoryComedyMovies} />
-        <Row title="Horror Movies" fetchUrl={requests.categoryHorrorMovies} />
-        <Row title="Romance Movies" fetchUrl={requests.categoryRomanceMovies} />
-        <Row title="Documentaries" fetchUrl={requests.categoryDocumenttaries} />
-      </div>
-    </>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.categoryNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Top Movies" fetchUrl={requests.categoryTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.categoryActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.categoryComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.categoryHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.categoryRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.categoryDocumenttaries} />
+    </div>
   );
 }
