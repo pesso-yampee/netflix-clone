a99adf51b6f036a37f674c0f5cca30f532777ec0
yamasaki junpei <opss0622@gmail.com>
2023-03-26 10:05:47 +0900

    ♻️ スクロールイベントに関わる処理を最適化, スタイルを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import logo from "assets/media/images/header_logo.png";
-export default function Header(): JSX.Element {
+
+export const Header = (): JSX.Element => {
   const [isScrolled, setIsScrolled] = useState<Boolean>(false);
 
   function toggleBgColor() {
@@ -8,20 +9,23 @@ export default function Header(): JSX.Element {
   }
 
   useEffect(() => {
-    window.addEventListener('scroll', toggleBgColor)
-  }, [])
+    window.addEventListener("scroll", toggleBgColor);
+    return () => {
+      window.removeEventListener("scroll", toggleBgColor);
+    };
+  }, []);
 
   return (
     <>
       <header className="fixed top-0 left-0 z-10 w-full">
         <div
-          className={`trasition-all flex items-center justify-between p-2 duration-300
+          className={`trasition-all flex items-center justify-between p-2.5 pr-4 duration-300
             ${isScrolled ? "bg-black" : "bg-transparent"}`}
         >
           <img src={logo} alt="" width={100} />
-          <div className="  h-10 w-10 bg-green-800 "></div>
+          <div className="  h-8 w-8 bg-green-800 "></div>
         </div>
       </header>
     </>
   );
-}
+};
