5e59730136cb4d01b4694d4647118fb34f648511
yamasaki junpei <opss0622@gmail.com>
2023-03-29 22:26:34 +0900

    ✨ Youtube動画を表示する機能を追加

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,10 +1,6 @@
-// 仕様
-// axiosを用いて非同期処理でデータを取得する
-// コンテンツは横スクロールができる
-// ホバーすると画像が拡大する
-
 import { useEffect, useState } from "react";
 import instance from "lib/api/axios";
+import YouTube from "react-youtube";
 
 type Props = {
   title: string;
@@ -12,16 +8,25 @@ type Props = {
   isLargeRow?: boolean;
 };
 
-type movie = {
+type Movie = {
   id: number;
   name: string;
   original_name: string;
-  backdrop_path: string;
-  poster_path: string;
+  backdrop_path?: string;
+  poster_path?: string;
+};
+
+type Options = {
+  width: string;
+  height: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  };
 };
 
 export const Row = ({ title, fetchUrl, isLargeRow }: Props): JSX.Element => {
-  const [movies, setMovies] = useState<movie[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("");
   const baseUrl = "https://image.tmdb.org/t/p/original";
 
   useEffect(() => {
@@ -33,24 +38,58 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props): JSX.Element => {
     fetchData();
   }, [fetchUrl]);
 
+  const opts: Options = {
+    width: "640",
+    height: "390",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie: Movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerUrl = await instance.get(
+        `/movie/${movie.id}/videos?api_key=5927808c5dcf9c9db491fa5372086df8`
+      );
+      setTrailerUrl(trailerUrl.data.results[0]?.key);
+    }
+  };
+
   return (
-    <div>
-      <h2 className="font-serif text-2xl text-white">{title}</h2>
-      <ul className="flex gap-4 overflow-y-hidden overflow-x-scroll pt-4 pl-6">
-        {movies.map((movie: movie) => (
-          <li key={movie.id}>
-            <figure className={isLargeRow ? "w-64" : "w-20"}>
+    <div className="overflow-x-hidden">
+      <h2 className="text-2xl font-bold text-white">{title}</h2>
+      <ul className="hidden-scrollbar mt-7 flex gap-5 overflow-x-scroll pl-7">
+        {movies.map((movie: Movie) => (
+          <li
+            key={movie.id}
+            tabIndex={0}
+            className=" cursor-pointer duration-500 hover:scale-[1.08]"
+          >
+            <figure
+              className={`${
+                isLargeRow
+                  ? "w-[165px] [&>img]:h-full"
+                  : "h-[calc(9/16*180px)] w-[180px] overflow-hidden"
+              }`}
+            >
               <img
-                style={{ maxWidth: "initial", width: "100%" }}
                 src={`${baseUrl}${
-                  isLargeRow ? movie.poster_path ?? movie.backdrop_path : movie.backdrop_path
+                  isLargeRow
+                    ? movie.poster_path
+                    : movie.backdrop_path ?? movie.poster_path
                 }`}
                 alt={movie.name}
+                onClick={() => handleClick(movie)}
               />
             </figure>
           </li>
         ))}
       </ul>
+      {trailerUrl && (
+        <YouTube className="mt-7" videoId={trailerUrl} opts={opts} />
+      )}
     </div>
   );
 };
